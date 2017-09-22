db81c8c4a4a737858bb9a06665e8da15f8fa1869
Richard Piazza <richard@richardpiazza.com>
2017-09-22 17:06:43 -0500

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Cbz2",
+    pkgConfig: "libbz2",
+    dependencies: []
+)
---
+++ README.md
@@ -0,0 +1,7 @@
+# Cbz2
+
+This is a Swift package manager module for bzip2. You can install bzip2 with brew (`brew install bzip2`).
+
+To use, add as a package dependency:
+
+    .package(url: "https://github.com/richardpiazza/Cbz2.git", from: "1.0.0")
